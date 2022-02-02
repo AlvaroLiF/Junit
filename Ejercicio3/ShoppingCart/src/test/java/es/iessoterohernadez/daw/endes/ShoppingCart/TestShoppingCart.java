976b55eed32b63a26983603067d43017c7a27d4e
AlvaroLiF <93470137+AlvaroLiF@users.noreply.github.com>
2022-02-02 21:29:24 +0100

    Add files via upload

---
+++ Ejercicio3/ShoppingCart/src/test/java/es/iessoterohernadez/daw/endes/ShoppingCart/TestShoppingCart.java
@@ -0,0 +1,71 @@
+package es.iessoterohernadez.daw.endes.ShoppingCart;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class TestShoppingCart {
+	
+	private static ShoppingCart carro;
+	private static Product producto;
+	
+	@BeforeAll
+	static void Iniciar() {
+		carro = new ShoppingCart();
+		producto = new Product("Chocolate",1.29);
+	}
+	
+	@AfterAll
+	static void Anular() {
+		carro = null;
+	}
+	
+	@Test
+	public void TestCrearCarro() {
+		assertEquals(0,carro.getItemCount());
+	}
+
+	@Test
+	public void TestCarroVacio() {
+		carro.addItem(producto);
+		carro.empty();
+		assertEquals(0,carro.getItemCount());
+	}
+	
+	@Test
+	public void TestAñadirProducto() {
+		carro.addItem(producto);
+		assertEquals(2,carro.getItemCount());
+	}
+	
+	@Test
+	public void TestBalance() {
+		carro.addItem(producto);
+		assertEquals(1.29,carro.getBalance());
+	}
+	
+	@Test
+	public void TestEliminarProducto() throws ProductNotFoundException {
+		carro.addItem(producto);
+		carro.removeItem(producto);
+		assertEquals(2,carro.getItemCount());
+	}
+	
+	
+	@Test
+	public void TestEliminarProductoFueraDelCarro() {
+		carro.addItem(producto);
+		try {
+			carro.removeItem(producto);
+		} catch (ProductNotFoundException e) {
+			fail();
+		}
+		assertEquals(0, carro.getItemCount());
+	}
+}
